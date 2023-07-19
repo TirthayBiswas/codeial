0312ba08e6df1f4e7ec517027d52bff1c307ec82
Tirthay Biswas <tirthay09phsa@gmail.com>
2023-07-19 18:16:36 +0530

    setup static files access for layouts and pages

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ const homeController=require('../controllers/home.controller');
 //app.use
 console.log('router loaded');
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
+
+//for any further routes ,acess from here
+//router.use('/routerName ',require('./routerfile));
 
 module.exports=router;
